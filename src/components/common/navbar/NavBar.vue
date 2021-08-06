08d617e24eab8834ea7c203bc56e2458549dd67d
zhmiao <zhmiao520@163.com>
2021-08-06 15:02:01 +0800

    1

--- src/components/common/navbar/NavBar.vue
+++ src/components/common/navbar/NavBar.vue
@@ -24,7 +24,7 @@
     height: 44px;
     line-height: 44px;
 
-    box-shadow: 0 1px 1px 1px rgba(100, 100, 100, .1);
+    box-shadow: 0 1px 1px rgba(100, 100, 100, .1);
   }
   .left, .right {
     width: 60px;
